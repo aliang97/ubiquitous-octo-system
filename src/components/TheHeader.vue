bb0807d3540c72131638ecacd02a744f3ea43cf7
Alex Liang <notmyworkemail3@gmail.com>
2025-02-13 02:25:25 -0500

    initial commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="header">
+    This is a header
+  </header>
+</template>
+
+<style scoped>
+.header {
+  background-color: red;
+}
+</style>
